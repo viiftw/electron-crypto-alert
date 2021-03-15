9a71407f4d01b9019ce4ad338c2f1dd42e72ab31
viiftw <datdn.it@gmail.com>
2021-03-15 11:20:15 +0700

    init project

---
+++ README.md
@@ -0,0 +1,26 @@
+# A simple crypto app using electron
+
+Notify if BTC price target is met
+
+## What did i learn
+
+- Installing Electron
+- Working with Electron Menus
+- Integrating an HTTP Library for making API Calls
+- Communicating between multiple windows with IPC
+- Accessing Native Desktop Functionality
+- Styling a Electron App
+- Deploying Electron App
+
+## Dev
+
+```bash
+npm install
+npm start
+```
+
+## Deploy
+
+```bash
+npm run package-win    (or package-mac, package-linux)
+```
---
+++ main.js
@@ -0,0 +1,87 @@
+require('electron-reload')(__dirname, {
+  // Note that the path to electron may vary according to the main file
+  electron: require(`${__dirname}/node_modules/electron`)
+})
+const {app, BrowserWindow, Menu} = require('electron')
+// To open a browser window, we first have to import the Electron shell
+const shell = require('electron').shell
+const ipc = require('electron').ipcMain
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win
+
+function createWindow () {
+  // Create the browser window.
+  win = new BrowserWindow({ width: 800, height: 600 })
+
+  // and load the index.html of the app.
+  win.loadFile('src/index.html')
+
+  // Open the DevTools.
+  win.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  win.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    win = null
+  })
+
+  var menu = Menu.buildFromTemplate([
+    {
+        label: 'Menu',
+        submenu: [
+            {label:'Adjust Notification Value'},
+            {
+              label:'CoinMarketCap',
+              click() { 
+                  shell.openExternal('http://coinmarketcap.com')
+              },
+              accelerator: 'CmdOrCtrl+Shift+C'// define a keyboard shortcut to simulate a click event on a submenu item
+            },
+            {type:'separator'},// create a line that separates
+            {
+              label:'Exit', 
+              click() { 
+                  app.quit() 
+              } 
+            }
+        ]
+    },
+    {
+        label: 'Info'
+    }
+  ])
+  Menu.setApplicationMenu(menu)
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+    createWindow()
+  }
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
+
+ipc.on('update-notify-value', function (event, arg) {
+  win.webContents.send('targetPriceVal', arg)
+})
